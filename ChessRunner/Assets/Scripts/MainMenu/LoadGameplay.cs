37a83928a727e698c4988d14dfaab71157672ab0
Falme Streamless <falme_shyness@hotmail.com>
2019-07-15 00:21:52 -0300

    Criado Função do popup de gameover

---
+++ ChessRunner/Assets/Scripts/MainMenu/LoadGameplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGameplay : MonoBehaviour
+{
+	public void LoadSceneGameplay()
+	{
+		StartCoroutine(LoadGameplayAsync());
+	}
+
+
+	public void ReloadSceneGameplay()
+	{
+		StartCoroutine(LoadGameplayAsync());
+	}
+
+	IEnumerator LoadGameplayAsync()
+	{
+		Scene actualScene = SceneManager.GetActiveScene();
+
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+
+		while (!asyncLoad.isDone)
+		{
+			yield return null;
+		}
+
+		yield return new WaitForSeconds(1f);
+
+		SceneManager.UnloadScene(actualScene);
+	}
+}
--- ChessRunner/Assets/Scripts/MainMenu/MainMenuManager.cs
+++ ChessRunner/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -6,25 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-	public void GoToGameplay()
-	{
-		StartCoroutine(LoadGameplayAsync());
-
-	}
-
-	IEnumerator LoadGameplayAsync()
-	{
-
-		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
-
-		while(!asyncLoad.isDone)
-		{
-			yield return null;
-		}
-
-		yield return new WaitForSeconds(1f);
-
-		SceneManager.UnloadScene(0);
-	}
+	
 
 }
