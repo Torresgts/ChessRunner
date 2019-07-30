353057111a6f757f83eddbb8571ad0a5e2ab35b0
Falme Streamless <falme_shyness@hotmail.com>
2019-07-30 00:25:56 -0300

    Adicionado Experiencia com numero de movimentos

--- ChessRunner/Assets/Scripts/Gameplay/GameplayManager.cs
+++ ChessRunner/Assets/Scripts/Gameplay/GameplayManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class GameplayManager : MonoBehaviour
 {
+    public static int points = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        points = 0;
     }
 
     // Update is called once per frame
